d105ebeef09fe0796f9f2fb108e7f172b7651fe4
dovewi <iidove@live.cn>
2013-11-12 14:59:27 +0800

    update by dovewi
    更新登录界面代码

--- src/com/dovewi/bookmgrcode/ui/LoginForm.java
+++ src/com/dovewi/bookmgrcode/ui/LoginForm.java
@@ -23,40 +23,40 @@ public class LoginForm {
 		jFrame.setResizable(false);
 		jFrame.setLayout(null);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel label1 = new JLabel("姓名");
+
+		JLabel label1 = new JLabel("账号");
 		label1.setBounds(10, 10, 100, 30);
 		jFrame.add(label1);
-		
+
 		JLabel label2 = new JLabel("密码");
 		label2.setBounds(10, 40, 100, 30);
 		jFrame.add(label2);
-		
+
 		final JTextField text1 = new JTextField();
 		text1.setBounds(50, 15, 130, 20);
 		jFrame.add(text1);
-		
+
 		final JPasswordField text2 = new JPasswordField();
 		text2.setBounds(50, 45, 130, 20);
 		jFrame.add(text2);
-		
+
 		JButton button = new JButton("Login");
 		button.setBounds(10, 75, 170, 40);
 		button.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(userName==text1.getText().toString() && passwrod==text2.getPassword().toString()) {
-					JOptionPane.showMessageDialog(null, "登陆成功误", "提示", JOptionPane.INFORMATION_MESSAGE);
+				if(userName.equals(text1.getText()) && passwrod.equals(String.valueOf(text2.getPassword()))) {
+					JOptionPane.showMessageDialog(null, "登陆成功", "提示", JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "错误", "提示", JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(null, "登录错误", "提示", JOptionPane.ERROR_MESSAGE);
 					text1.setText("");
 					text2.setText("");
 				}
 			}
 		});
 		jFrame.add(button);
-		
+
 		jFrame.setVisible(true);
 	}
 
