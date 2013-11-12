bc1ec3ef267e13fb264d850babb31cffc2f2f56b
dovewi <iidove@live.cn>
2013-11-12 14:52:17 +0800

    by dovewi
    修改了登录窗口代码

--- src/com/dovewi/bookmgrcode/ui/LoginForm.java
+++ src/com/dovewi/bookmgrcode/ui/LoginForm.java
@@ -1,20 +1,63 @@
 package com.dovewi.bookmgrcode.ui;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-public class LoginForm extends JFrame implements ActionListener{
-	//TODO add a LoginForm for this project,it needs a textField and passwordField
-	//TODO add two button,Login and exit
-	//TODO click exit to System.exit(0);
-	LoginForm(){
-		//TODO new FORM
+public class LoginForm {
+
+	public static void main(String[] args) {
+		final String userName = "abc";
+		final String passwrod = "123";
+		JFrame jFrame = new JFrame("管理员登录");
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		jFrame.setBounds(((int)dimension.getWidth() - 200) / 2, ((int)dimension.getHeight() - 300) / 2, 200, 150);
+		jFrame.setResizable(false);
+		jFrame.setLayout(null);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel label1 = new JLabel("姓名");
+		label1.setBounds(10, 10, 100, 30);
+		jFrame.add(label1);
+		
+		JLabel label2 = new JLabel("密码");
+		label2.setBounds(10, 40, 100, 30);
+		jFrame.add(label2);
+		
+		final JTextField text1 = new JTextField();
+		text1.setBounds(50, 15, 130, 20);
+		jFrame.add(text1);
+		
+		final JPasswordField text2 = new JPasswordField();
+		text2.setBounds(50, 45, 130, 20);
+		jFrame.add(text2);
+		
+		JButton button = new JButton("Login");
+		button.setBounds(10, 75, 170, 40);
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(userName==text1.getText().toString() && passwrod==text2.getPassword().toString()) {
+					JOptionPane.showMessageDialog(null, "登陆成功误", "提示", JOptionPane.INFORMATION_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(null, "错误", "提示", JOptionPane.ERROR_MESSAGE);
+					text1.setText("");
+					text2.setText("");
+				}
+			}
+		});
+		jFrame.add(button);
+		
+		jFrame.setVisible(true);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		// TODO add the events
-	}
 }
